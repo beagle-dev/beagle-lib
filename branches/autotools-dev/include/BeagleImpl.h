efc4a3a520a048f2193ca0420b17c8f0e84e2440
aaron.darling <aaron.darling@6a382a6a-8faa-5fa0-d5e7-fc71f7735a90>
2009-05-20 02:27:06 +0000

    creating a branch of trunk for integration with autotools and GNU library standards

---
+++ branches/autotools-dev/include/BeagleImpl.h
@@ -0,0 +1,88 @@
+/*
+ *  BeagleImpl.h
+ *  BEAGLE
+ *
+ * @author Andrew Rambaut
+ * @author Marc Suchard
+ *
+ */
+
+#ifndef __beagle_impl__
+#define __beagle_impl__
+
+#ifdef DOUBLE_PRECISION
+	#define REAL	double
+#else
+	#define REAL	float
+#endif
+
+#define SUCCESS	1
+#define ERROR	0
+
+class BeagleImpl {
+public:
+	virtual int initialize(
+					int nodeCount,
+					int tipCount,
+					int stateCount,
+					int patternCount,
+					int categoryCount,
+					int matrixCount) = 0;
+
+	virtual void finalize() = 0;
+
+	virtual void setTipPartials(
+						int tipIndex,
+						double* inPartials) = 0;
+
+	virtual void setTipStates(
+					  int tipIndex,
+					  int* inStates) = 0;
+
+	virtual void setStateFrequencies(double* inStateFrequencies) = 0;
+
+	virtual void setEigenDecomposition(
+							   int matrixIndex,
+							   double** inEigenVectors,
+							   double** inInverseEigenVectors,
+							   double* inEigenValues) = 0;
+
+	virtual void setCategoryRates(double* inCategoryRates) = 0;
+
+	virtual void setCategoryProportions(double* inCategoryProportions) = 0;
+
+	virtual void calculateProbabilityTransitionMatrices(
+												int* nodeIndices,
+												double* branchLengths,
+												int count) = 0;
+
+	virtual void calculatePartials(
+						   int* operations,
+						   int* dependencies,
+						   int count,
+						   int rescale) = 0;
+
+	virtual void calculateLogLikelihoods(
+								 int rootNodeIndex,
+								 double* outLogLikelihoods) = 0;
+
+	virtual void storeState() = 0;
+
+	virtual void restoreState() = 0;
+
+};
+
+class BeagleImplFactory {
+    public:
+	virtual BeagleImpl* createImpl(
+							int nodeCount,
+							int tipCount,
+							int stateCount,
+							int patternCount,
+							int categoryCount,
+							int matrixCount) = 0; // pure virtual
+
+	virtual const char* getName() = 0; // pure virtual
+};
+
+#endif // __beagle_impl__
---
+++ branches/autotools-dev/java/beagle/BeagleFactory.java
@@ -0,0 +1,91 @@
+/**
+ *
+ */
+package beagle;
+
+import java.util.*;
+
+/**
+ * @author Marc Suchard
+ * @author Andrew Rambaut
+ *
+ */
+public class BeagleFactory {
+
+    public static final String STATE_COUNT = "stateCount";
+    public static final String PREFER_SINGLE_PRECISION = "preferS inglePrecision";
+    public static final String SINGLE_PRECISION = "singlePrecision";
+    public static final String DEVICE_NUMBER = "deviceNumber";
+
+    private static Beagle load(BeagleLoader loader, Map<String, Object> configuration) {
+        // configuration.put(BeagleFactory.SINGLE_PRECISION, singlePrecision);
+
+        System.out.print("Attempting to load beagle: " + loader.getLibraryName(configuration));
+
+        Beagle beagle = loader.createInstance(configuration);
+
+        if (beagle != null) {
+            System.out.println(" - SUCCESS");
+
+            return beagle;
+        }
+        System.out.println(" - FAILED");
+        return null;
+    }
+
+    public static Beagle loadBeagleInstance(Map<String, Object> configuration) {
+
+        if (registry == null) {  // Lazy loading
+            registry = new ArrayList<BeagleLoader>();  // List libraries in order of load-priority
+            registry.add(new BeagleJNIWrapper.BeagleLoader());
+        }
+
+        for(BeagleLoader loader: registry) {
+
+            // Try prefered precision library
+            Beagle beagle = load(loader, configuration);
+            if (beagle != null)
+                return beagle;
+        }
+
+        // No libraries/processes available
+
+        int stateCount = (Integer)configuration.get(STATE_COUNT);
+//        boolean singlePrecision = (Boolean)configuration.get(PREFER_SINGLE_PRECISION);
+//
+//        if (singlePrecision) {
+//            // return new SinglePrecisionBeagleImpl(stateCount);
+//            // throw new UnsupportedOperationException("Single precision Java version of BEAGLE not implemented");
+//            System.out.println("Single precision Java version of BEAGLE not available; defaulting to double precision");
+//        } // else {
+
+        if (stateCount == 4) {
+//            return new DependencyAwareBeagleImpl();
+            return new FourStateBeagleImpl();
+        }
+        return new GeneralBeagleImpl(stateCount);
+    }
+
+    private static List<BeagleLoader> registry;
+
+    protected interface BeagleLoader {
+        public String getLibraryName(Map<String, Object> configuration);
+
+        /**
+         * Actual factory
+         * @param configuration
+         * @return
+         */
+        public Beagle createInstance(Map<String, Object> configuration);
+    }
+
+    public static void main(String[] argv) {
+        Map<String, Object> configuration = new HashMap<String, Object>();
+        configuration.put(BeagleFactory.STATE_COUNT, 4);
+        configuration.put(BeagleFactory.PREFER_SINGLE_PRECISION, false);
+        configuration.put(BeagleFactory.DEVICE_NUMBER, 0);
+
+        Beagle instance = BeagleFactory.loadBeagleInstance(configuration);
+    }
+
+}
