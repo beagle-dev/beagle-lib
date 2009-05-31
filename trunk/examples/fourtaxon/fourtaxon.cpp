b3f539c6e46950d9c04a15df8838aa05c9539c30
paul.o.lewis <paul.o.lewis@6a382a6a-8faa-5fa0-d5e7-fc71f7735a90>
2009-05-31 16:37:56 +0000

    Cleaned up fourtaxon example (added documentation and option to specify niters and data file name on command line)

--- trunk/examples/fourtaxon/fourtaxon.cpp
+++ trunk/examples/fourtaxon/fourtaxon.cpp
@@ -1,6 +1,8 @@
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <numeric>	// needed for accumulate algorithm
 #include "fourtaxon.hpp"
-#include <algorithm>
-using namespace std;
 
 /*-----------------------------------------------------------------------------
 | 	Allocates a two-dimensional array of doubles as one contiguous block of 
@@ -35,8 +37,8 @@ void DeleteTwoDArray (double ** & ptr)
 |	Constructor simply calls init().
 */
 FourTaxonExample::FourTaxonExample()
+  : ntaxa(4), niters(0), nsites(0), instance_handle(-1)
 	{
-	//init();
 	}
 	
 /*-----------------------------------------------------------------------------
@@ -51,10 +53,9 @@ void FourTaxonExample::abort(
 	}
 	
 /*-----------------------------------------------------------------------------
-|	The init() function sets up the beagle library and initializes all data
-|	members.
+|	This function sets up the beagle library and initializes all data members.
 */
-void FourTaxonExample::init()
+void FourTaxonExample::initBeagleLib()
 	{
 	int code;
 	partial.resize(4);
@@ -75,9 +76,7 @@ void FourTaxonExample::init()
 	operations.push_back(2);	// left child transition matrix index
 	operations.push_back(3);	// right child partial index
 	operations.push_back(3);	// right child transition matrix index
-	
-	cout << "nsites\t" << nsites << endl;
-	
+		
 	instance_handle = createInstance(
 				4,			// tipCount
 				7,			// partialsBufferCount
@@ -96,13 +95,18 @@ void FourTaxonExample::init()
 		abort("createInstance returned a negative instance handle (and that's not good)");
 				
 	transition_matrix_index.resize(5);
+	transition_matrix_index[0] = 0;
+	transition_matrix_index[1] = 1;
+	transition_matrix_index[2] = 2;
+	transition_matrix_index[3] = 3;
+	transition_matrix_index[4] = 4;
+
 	brlens.resize(5);
-	for (unsigned brIndex = 0; brIndex < 5; ++brIndex)
-		{
-		transition_matrix_index[brIndex] = brIndex;
-		brlens[brIndex] = 0.01 + ((float)brIndex)/100.0;
-		}
-   
+	brlens[0] = 0.01;
+	brlens[1] = 0.02;
+	brlens[2] = 0.03;
+	brlens[3] = 0.04;
+	brlens[4] = 0.05;
 	
 	for (unsigned i = 0; i < 4; ++i)
 		{
@@ -113,6 +117,8 @@ void FourTaxonExample::init()
 		if (code != 0)
 			abort("setPartials encountered a problem");
 		}
+		
+	// JC69 model eigenvector matrix
 	double evec[4][4] = {
 		{ 1.0,  2.0,  0.0,  0.5},
 		{ 1.0,  -2.0,  0.5,  0.0},
@@ -120,6 +126,7 @@ void FourTaxonExample::init()
 		{ 1.0,  -2.0,  -0.5,  0.0}
 	};
 	
+	// JC69 model inverse eigenvector matrix
 	double ivec[4][4] = {
 		{ 0.25,  0.25,  0.25,  0.25},
 		{ 0.125,  -0.125,  0.125,  -0.125},
@@ -127,8 +134,10 @@ void FourTaxonExample::init()
 		{ 1.0,  0.0,  -1.0,  0.0}
 	};
 	
+	// JC69 model eigenvalues
 	double eval[4] = { 0.0, -1.3333333333333333, -1.3333333333333333, -1.3333333333333333 };
 	
+	// Creating of temporary two-dimensional matrices is necessary because library expects double **
 	double ** evecP = NewTwoDArray(4,4);
 	double ** ivecP = NewTwoDArray(4,4);
 
@@ -154,7 +163,8 @@ void FourTaxonExample::init()
 	}
 	
 /*-----------------------------------------------------------------------------
-|	
+|	Calculates the log likelihood by calling the beagle functions 
+|	updateTransitionMatrices, updatePartials and calculateEdgeLogLikelihoods.
 */
 double FourTaxonExample::calcLnL()
 	{
@@ -184,12 +194,12 @@ double FourTaxonExample::calcLnL()
 	int childBufferIndex  = 5;
 	int transitionMatrixIndex  = 4;
 	double relativeRateProb  = 1.0;
-	//double stateFreqs[] = {0.25, 0.25, 0.25, 0.25};
+
 	double ** stateFreqs = NewTwoDArray(1,4);
 	for (int i=0;i<4;i++)
 		stateFreqs[0][i] = 0.25;
 
-	vector<double> lnL(nsites);
+	std::vector<double> lnL(nsites);
 	
 	code = calculateEdgeLogLikelihoods(
 		 instance_handle,					// instance,
@@ -205,52 +215,58 @@ double FourTaxonExample::calcLnL()
 		 NULL,								// outFirstDerivatives,
 		 NULL);								// outSecondDerivatives
 
+	DeleteTwoDArray(stateFreqs);
+
 	if (code != 0)
 		abort("calculateEdgeLogLikelihoods encountered a problem");
 		
-	//return std::accumulate(lnL.begin(),lnL.end(),0.0);
-	double tot=0.0;
-	for(int i=0;i<nsites;i++){
-//		std::cout << lnL[i] << std::endl;
-		tot += lnL[i];
-		}
-	return tot;
-//	return lnL;
+	return std::accumulate(lnL.begin(), lnL.end(), 0.0);
 	}
 
 /*-----------------------------------------------------------------------------
-|	
+|	Reads in the data file (which must be named fourtaxon.dat and must contain
+|	data for four taxa), then calls calcLnL `n' times before calling finalize
+|	to inform the beagle library that it is ok to destroy all allocated memory.
 */
 void FourTaxonExample::run()
 	{
-	readData("fourtaxon.dat");
-	init();
-	//writeData("example_data.check.txt");
+	readData();
+	initBeagleLib();
+	writeData();
+	
+	std::cout.setf(std::ios::showpoint);
+	std::cout.setf(std::ios::floatfield, std::ios::fixed);
 
-	unsigned nreps = 1;
-	for (unsigned rep = 0; rep < nreps; ++rep)
+	std::cout << std::setw(12) << "iter" << std::setw(24) << "log-likelihood" << std::endl;
+	for (unsigned rep = 1; rep <= niters; ++rep)
 		{
-		std::cerr << rep << ": lnL = " << calcLnL() << std::endl;
+		std::cout << std::setw(12) << rep;
+		std::cout << std::setw(24) << std::setprecision(5) << calcLnL() << std::endl;
 		}
 		
 	int code = finalize(
 		instance_handle);		// instance
-	
-	std::cerr << "finalize returned " << code << std::endl;
+		
+	if (code != 0)
+		abort("finalize encountered a problem");
 	}
 
 /*-----------------------------------------------------------------------------
-|	
+|	Reads the data file the name of which is supplied. This function expects
+|	the data to be in pseufoPHYLIP format: ntaxa followed by nsites on first 
+|	line, then name and sequence data (separated by whitespace) for each 
+|	taxon on subsequent lines. Converts the DNA states A, C, G, and T to int
+|	codes 0, 1, 2 and 3, respectively, storing these in the `data' data member.
+|	Also calculates the partials for the tip nodes, which are stored in the 
+|	`partial' data member.
 */
-void FourTaxonExample::readData(const std::string file_name)
+void FourTaxonExample::readData()
 	{
-	std::cerr << "opening " << file_name.c_str() << " ..." << std::endl;
 	std::string sequence;
-	std::ifstream inf(file_name.c_str());
+	std::ifstream inf(data_file_name.c_str());
 	if(!inf.good())
-		abort("problem reading datafile");
-	inf >> ntaxa >> nsites;
-	cout << "ntaxa\t" << ntaxa << "\tnsites\t" << nsites << endl;
+		abort("problem reading data file");
+	inf >> nsites;	// ntaxa is a constant (4) in this example
 	taxon_name.resize(ntaxa);
 	data.resize(ntaxa);
 	partial.resize(ntaxa);
@@ -309,15 +325,20 @@ void FourTaxonExample::readData(const std::string file_name)
 	}
 	
 /*-----------------------------------------------------------------------------
-|	
+|	This function spits out the data as a nexus formatted data file with a 
+|	paup block that can be used to check the starting likelihood in PAUP*.
 */
-void FourTaxonExample::writeData(const std::string file_name)
+void FourTaxonExample::writeData()
 	{
-	std::ofstream outf(file_name.c_str(), std::ios::out);
-	outf << ntaxa << " " << nsites << std::endl;
+	std::ofstream outf("check_lnL_using_paup.nex", std::ios::out);
+	outf << "#nexus\n\n";
+	outf << "begin data;\n";
+	outf << "  dimensions ntax=" << ntaxa << " nchar=" << nsites << ";\n";
+	outf << "  format datatype=dna missing=? gap=-;\n";
+	outf << "  matrix\n";
 	for (unsigned i = 0; i < ntaxa; ++i)
 		{
-		outf << taxon_name[i] << " ";
+		outf << "    " << taxon_name[i] << "\t\t\t";
 		for (unsigned j = 0; j < nsites; ++j)
 			{
 			switch (data[i][j])
@@ -340,15 +361,63 @@ void FourTaxonExample::writeData(const std::string file_name)
 			}
 			outf << std::endl;
 		}
+	outf << "  ;\n";
+	outf << "end;\n\n";
+	outf << "begin paup;\n";
+	outf << "  set criterion=likelihood storebrlens;\n";
+	outf << "end;\n\n";
+	outf << "begin trees;\n";
+	outf << "  tree starting = [&U](alga_D86836:0.01, fern_D14882:0.02, (hops_AF206777:0.03, corn_Z11973:0.04):0.05);\n";
+	outf << "end;\n\n";
+	outf << "begin paup;\n";
+	outf << "  lset nst=1 basefreq=equal;\n";
+	outf << "  lscores 1 / userbrlen;\n";
+	outf << "end;\n";
 	outf.close();
 	}
-
+	
 /*-----------------------------------------------------------------------------
-|	
+|	Reads command line arguments and interprets them as follows:
+|>
+|	fourtaxon [<niters> [<data_file_name>]]
+|>
+|	If niters is not specified, the default value 1 million is used.
+|	If data_file_name	is not specified, the default value "fourtaxon.dat" is
+|	used. If data_file_name is specified, the file should have the same format
+|	as the file "fourtaxon.dat" and should only contain sequences for 4 taxa
+|	(although the sequences can be of arbitrary length).
 */
-int main(int argc, char* argv[])
+void FourTaxonExample::interpretCommandLineParameters(
+  int argc, 		/**< is the number of command line arguments */
+  char* argv[])		/**< is the array of command line arguments */
 	{
-	FourTaxonExample fte;
-	fte.run();
+	// see if the user specified the number of MCMC iterations on the command line
+	// and, if so, replace the default value of niters
+	niters = 1000000;
+	if (argc > 1)
+		niters = (unsigned)atoi(argv[1]);
+	if (niters < 1)
+		abort("invalid number of iterations supplied on the command line");
+	
+	// see if the user specified a data file name on the command line
+	// and, if so, replace the default value of data_file_name
+	data_file_name = "fourtaxon.dat";
+	if (argc > 2)
+		data_file_name = std::string(argv[2]);
+	}
+	
+/*-----------------------------------------------------------------------------
+|	Constructs a FourTaxonExample object, calls its 
+|	interpretCommandLineParameters to (optionally) change the number of
+|	iterations and data file name, and then calls the run() function to 
+|	initialize the beagle library and start the analysis.
+*/
+int main(
+  int argc, 		/**< is the number of command line arguments */
+  char* argv[])		/**< is the array of command line arguments */
+	{
+	FourTaxonExample fourtax;
+	fourtax.interpretCommandLineParameters(argc, argv);
+	fourtax.run();
 	}
 	
