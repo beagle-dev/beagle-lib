38f8010ad50f762cfd4cd18202aa9ea637d53af2
Aaron Darling <koadman@users.noreply.github.com>
2009-06-15 04:18:27 +0000

    Adding an example program that times the calculation of tree likelihood for a genome-size dataset on each available hardware resource (eg CPU, GPU).

---
+++ examples/genomictest/genomictest.cpp
@@ -0,0 +1,179 @@
+/*
+ *  genomictest.cpp
+ *  Created by Aaron Darling on 14/06/2009.
+ *  Based on tinyTest.cpp by Andrew Rambaut.
+ */
+#include <cstdio>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <sys/time.h>
+#include "libbeagle-lib/beagle.h"
+
+int ntaxa = 29;
+int nsites = 100000;
+// is nucleotides...
+int stateCount = 4;
+
+
+double* getRandomTipPartials( int nsites, int stateCount )
+{
+	double *partials = (double*)malloc(sizeof(double) * nsites * stateCount);
+	for( int i=0; i<nsites*stateCount; i+=stateCount )
+	{
+		int s = rand()%stateCount;
+		partials[i+s]=1.0;
+	}
+	return partials;
+}
+
+
+double runBeagle(ResourceList* resources)
+{
+	int* rl = NULL;
+	int rll = 0;
+	if(resources!=NULL){
+		rl = (int*)resources->list;
+		rll = resources->length;
+	}
+    // create an instance of the BEAGLE library
+	int instance = createInstance(
+			    ntaxa,			/**< Number of tip data elements (input) */
+				2*ntaxa-1,	        /**< Number of partials buffers to create (input) */
+				0,		        /**< Number of compact state representation buffers to create (input) */
+				stateCount,		/**< Number of states in the continuous-time Markov chain (input) */
+				nsites,			/**< Number of site patterns to be handled by the instance (input) */
+				1,		        /**< Number of rate matrix eigen-decomposition buffers to allocate (input) */
+				2*ntaxa-2,	        /**< Number of rate matrix buffers (input) */
+				rl,			/**< List of potential resource on which this instance is allowed (input, NULL implies no restriction */
+				rll,			/**< Length of resourceList list (input) */
+				0,		        /**< Bit-flags indicating preferred implementation charactertistics, see BeagleFlags (input) */
+				0		        /**< Bit-flags indicating required implementation characteristics, see BeagleFlags (input) */
+				);
+    if (instance < 0) {
+	    fprintf(stderr, "Failed to obtain BEAGLE instance\n\n");
+	    exit(1);
+    }
+
+    // initialize the instance
+    int error = initializeInstance(instance, NULL);
+	
+    if (error < 0) {
+	    fprintf(stderr, "Failed to initialize BEAGLE instance\n\n");
+	    exit(1);
+    }
+    // set the sequences for each tip using partial likelihood arrays
+	srand(42);	// fix the random seed...
+	for(int i=0; i<ntaxa; i++)
+	{
+		setPartials(instance, i, getRandomTipPartials(nsites, stateCount));
+	}
+	
+    // create base frequency array
+	double freqs[4] = { 0.25, 0.25, 0.25, 0.25 };
+
+    // create an array containing site category weights
+	const double weights[1] = { 1.0 };
+
+	// an eigen decomposition for the JC69 model
+	double evec[4 * 4] = {
+		 1.0,  2.0,  0.0,  0.5,
+		 1.0,  -2.0,  0.5,  0.0,
+		 1.0,  2.0, 0.0,  -0.5,
+		 1.0,  -2.0,  -0.5,  0.0
+	};
+
+	double ivec[4 * 4] = {
+		 0.25,  0.25,  0.25,  0.25,
+		 0.125,  -0.125,  0.125,  -0.125,
+		 0.0,  1.0,  0.0,  -1.0,
+		 1.0,  0.0,  -1.0,  0.0
+	};
+
+	double eval[4] = { 0.0, -1.3333333333333333, -1.3333333333333333, -1.3333333333333333 };
+
+    // set the Eigen decomposition
+	setEigenDecomposition(instance, 0, evec, ivec, eval);
+
+    // a list of indices and edge lengths
+	int* nodeIndices = new int[ntaxa*2-2];
+	for(int i=0; i<ntaxa*2-2; i++) nodeIndices[i]=i;
+	double* edgeLengths = new double[ntaxa*2-2];
+	for(int i=0; i<ntaxa*2-2; i++) edgeLengths[i]=0.1;
+
+    // tell BEAGLE to populate the transition matrices for the above edge lengths
+	updateTransitionMatrices(instance,     // instance
+	                         0,             // eigenIndex
+	                         nodeIndices,   // probabilityIndices
+	                         NULL,          // firstDerivativeIndices
+	                         NULL,          // secondDervativeIndices
+	                         edgeLengths,   // edgeLengths
+	                         ntaxa*2-2);            // count
+
+    // create a list of partial likelihood update operations
+    // the order is [dest, source1, matrix1, source2, matrix2]
+	int* operations = new int[(ntaxa-1)*5];
+	for(int i=0; i<ntaxa-1; i++){
+		operations[5*i+0] = ntaxa+i;
+		operations[5*i+1] = i*2;
+		operations[5*i+2] = i*2;
+		operations[5*i+3] = i*2+1;
+		operations[5*i+4] = i*2+1;
+	}	
+
+	int rootIndex = ntaxa*2-2;
+
+    // start timing!
+	struct timeval start, end;
+	gettimeofday(&start,NULL);
+
+    // update the partials
+	updatePartials( &instance,      // instance
+	                1,              // instanceCount
+	                operations,     // eigenIndex
+	                ntaxa-1,              // operationCount
+	                1);             // rescale ? 0 = no
+
+	double *patternLogLik = (double*)malloc(sizeof(double) * nsites);
+
+    // calculate the site likelihoods at the root node
+	calculateRootLogLikelihoods(instance,               // instance
+	                            (const int *)&rootIndex,// bufferIndices
+	                            weights,                // weights
+	                            freqs,                 // stateFrequencies
+	                            1,                      // count
+	                            patternLogLik);         // outLogLikelihoods
+
+	// end timing!
+	gettimeofday(&end,NULL);
+
+	double logL = 0.0;
+	for (int i = 0; i < nsites; i++) {
+		logL += patternLogLik[i];
+	}
+
+
+	fprintf(stdout, "logL = %.5f \n\n", logL);
+	double timediff =  end.tv_sec - start.tv_sec + (double)(end.tv_usec-start.tv_usec)/1000000.0;
+	std::cout << "Took " << timediff << " seconds\n";
+	finalize(instance);
+}
+
+int main( int argc, const char* argv[] )
+{
+	std::cout << "Simulating genomic DNA with " << ntaxa << " taxa and " << nsites << " site patterns\n";
+
+	ResourceList* rl = getResourceList();
+	if(rl != NULL){
+		for(int i=0; i<rl->length; i++){
+			ResourceList curlist;
+			curlist.list = new Resource[1];
+			curlist.list[0] = rl->list[i];
+			curlist.length = 1;
+			runBeagle(&curlist);
+			delete curlist.list;
+		}
+	}else{
+		runBeagle(NULL);
+	}
+}
