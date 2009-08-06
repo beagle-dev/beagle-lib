745c426615847681f327592b9611b2abc19f8054
rambaut <rambaut@users.noreply.github.com>
2009-08-06 11:44:16 +0000

    BeagleTreeLikelihood now prints info about the BEAGLE resource it is running on.

--- java/beagle/BeagleInfo.java
+++ java/beagle/BeagleInfo.java
@@ -1,6 +1,7 @@
 package beagle;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Marc A. Suchard
@@ -10,24 +11,11 @@ public class BeagleInfo {
 
     public static void printResourceList() {
 
-        ResourceDetails[] resourceDetails = BeagleFactory.getResourceDetails();
+        List<ResourceDetails> resourceDetails = BeagleFactory.getResourceDetails();
 
         System.out.println("BEAGLE resources available:");
         for (ResourceDetails resource : resourceDetails) {
-            System.out.println("\t" + resource.getNumber() + " : " + resource.getName());
-            String[] description = resource.getDescription().split("\\|");
-            for (String desc : description) {
-                if (desc.trim().length() > 0) {
-                    System.out.println("\t\t" +  desc.trim());
-                }
-            }
-            StringBuilder sb = new StringBuilder();
-            for (BeagleFlag flag : BeagleFlag.values()) {
-                if (flag.isSet(resource.getFlags())) {
-                    sb.append(" ").append(flag.name());
-                }
-            }
-            System.out.println("\t\tFlags:" + sb.toString());
+            System.out.println(resource.toString());
             System.out.println();
 
         }
