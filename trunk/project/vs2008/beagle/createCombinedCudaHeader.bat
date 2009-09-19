5bf0d0d5b5e6d4ecaff85421f581ef13a829cc98
msuchard <msuchard@6a382a6a-8faa-5fa0-d5e7-fc71f7735a90>
2009-09-19 00:33:20 +0000

    Windows GPU works.   Yeah...

--- trunk/project/vs2008/beagle/createCombinedCudaHeader.bat
+++
@@ -1,9 +0,0 @@
-del ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels4.ptx KERNELS_STRING_4 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels4ls.ptx KERNELS_STRING_LS_4 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels32.ptx KERNELS_STRING_32 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels32ls.ptx KERNELS_STRING_LS_32 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels48.ptx KERNELS_STRING_48 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels48ls.ptx KERNELS_STRING_LS_48 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels64.ptx KERNELS_STRING_64 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels64ls.ptx KERNELS_STRING_LS_64 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
