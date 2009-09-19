72a450029a94ea9ac8691b073332b7360e144008
Marc Suchard <msuchard@ucla.edu>
2009-09-19 21:10:43 +0000

    Still cant get custom build to call cudaBuildHelper

--- project/beagle-vs2008/createCombinedCudaHeader.bat
+++ project/beagle-vs2008/createCombinedCudaHeader.bat
@@ -1,9 +1,11 @@
-del ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels4.ptx KERNELS_STRING_4 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels4ls.ptx KERNELS_STRING_LS_4 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels32.ptx KERNELS_STRING_32 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels32ls.ptx KERNELS_STRING_LS_32 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels48.ptx KERNELS_STRING_48 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels48ls.ptx KERNELS_STRING_LS_48 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels64.ptx KERNELS_STRING_64 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels64ls.ptx KERNELS_STRING_LS_64 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
+set OUTFILE="..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h"
+if exist %OUTFILE% del %OUTFILE%
+
+cudaBuildHelper.exe data\kernels4.ptx KERNELS_STRING_4 %OUTFILE%
+cudaBuildHelper.exe data\kernels4ls.ptx KERNELS_STRING_LS_4 %OUTFILE%
+cudaBuildHelper.exe data\kernels32.ptx KERNELS_STRING_32 %OUTFILE%
+cudaBuildHelper.exe data\kernels32ls.ptx KERNELS_STRING_LS_32 %OUTFILE%
+cudaBuildHelper.exe data\kernels48.ptx KERNELS_STRING_48 %OUTFILE%
+cudaBuildHelper.exe data\kernels48ls.ptx KERNELS_STRING_LS_48 %OUTFILE%
+cudaBuildHelper.exe data\kernels64.ptx KERNELS_STRING_64 %OUTFILE%
+cudaBuildHelper.exe data\kernels64ls.ptx KERNELS_STRING_LS_64 %OUTFILE%
