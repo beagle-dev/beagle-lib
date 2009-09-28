173646c509ebc635ec2eaa266e521f89be459319
Aaron Darling <koadman@users.noreply.github.com>
2009-09-28 22:03:54 +0000

    In Windows build, replace usage of cudaBuildHelper with Nvidia's bin2c

--- project/beagle-vs2008/createCombinedCudaHeader.bat
+++ project/beagle-vs2008/createCombinedCudaHeader.bat
@@ -1,11 +1,11 @@
 set OUTFILE="..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h"
 if exist %OUTFILE% del %OUTFILE%
 
-cudaBuildHelper.exe data\kernels4.ptx KERNELS_STRING_4 %OUTFILE%
-cudaBuildHelper.exe data\kernels4ls.ptx KERNELS_STRING_LS_4 %OUTFILE%
-cudaBuildHelper.exe data\kernels32.ptx KERNELS_STRING_32 %OUTFILE%
-cudaBuildHelper.exe data\kernels32ls.ptx KERNELS_STRING_LS_32 %OUTFILE%
-cudaBuildHelper.exe data\kernels48.ptx KERNELS_STRING_48 %OUTFILE%
-cudaBuildHelper.exe data\kernels48ls.ptx KERNELS_STRING_LS_48 %OUTFILE%
-cudaBuildHelper.exe data\kernels64.ptx KERNELS_STRING_64 %OUTFILE%
-cudaBuildHelper.exe data\kernels64ls.ptx KERNELS_STRING_LS_64 %OUTFILE%
+bin2c.exe -n KERNELS_STRING_4 data\kernels4.ptx > %OUTFILE%
+bin2c.exe -n KERNELS_STRING_LS_4 data\kernels4ls.ptx >> %OUTFILE%
+bin2c.exe -n KERNELS_STRING_32 data\kernels32.ptx >> %OUTFILE%
+bin2c.exe -n KERNELS_STRING_LS_32 data\kernels32ls.ptx >> %OUTFILE%
+bin2c.exe -n KERNELS_STRING_48 data\kernels48.ptx >> %OUTFILE%
+bin2c.exe -n KERNELS_STRING_LS_48 data\kernels48ls.ptx >> %OUTFILE%
+bin2c.exe -n KERNELS_STRING_64 data\kernels64.ptx >> %OUTFILE%
+bin2c.exe -n KERNELS_STRING_LS_64 data\kernels64ls.ptx >> %OUTFILE%
