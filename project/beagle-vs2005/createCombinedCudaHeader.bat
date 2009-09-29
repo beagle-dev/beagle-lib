0b3f23e2f31047622461e53afc8d2a3e2e3c094e
Aaron Darling <koadman@users.noreply.github.com>
2009-09-29 19:18:02 +0000

    Updating vs2005 build -- automatic Java detection, build JNI files, use of Nvidia bin2c instead of my cudaBuildHelper.exe, adding eigen decomposition files

--- project/beagle-vs2005/createCombinedCudaHeader.bat
+++ project/beagle-vs2005/createCombinedCudaHeader.bat
@@ -1,9 +1,26 @@
-del ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels4.ptx KERNELS_STRING_4 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels4ls.ptx KERNELS_STRING_LS_4 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels32.ptx KERNELS_STRING_32 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels32ls.ptx KERNELS_STRING_LS_32 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels48.ptx KERNELS_STRING_48 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels48ls.ptx KERNELS_STRING_LS_48 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels64.ptx KERNELS_STRING_64 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
-cudaBuildHelper.exe data/kernels64ls.ptx KERNELS_STRING_LS_64 ..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h
+::
+:: windows script to create a single header with compiled CUDA 
+:: kernels defined as variables
+:: @author Aaron Darling
+::
+:: TODO: make this pretty with a loop over state counts
+::
+
+
+set OUTFILE="..\..\libhmsbeagle\GPU\kernels\BeagleCUDA_kernels.h"
+if exist %OUTFILE% del %OUTFILE%
+
+echo #ifndef __BeagleCUDA_kernels__ >> %OUTFILE%
+echo #define __BeagleCUDA_kernels__ >> %OUTFILE%
+
+bin2c.exe -st -n KERNELS_STRING_4 data\kernels4.ptx >> %OUTFILE%
+bin2c.exe -st -n KERNELS_STRING_LS_4 data\kernels4ls.ptx >> %OUTFILE%
+bin2c.exe -st -n KERNELS_STRING_32 data\kernels32.ptx >> %OUTFILE%
+bin2c.exe -st -n KERNELS_STRING_LS_32 data\kernels32ls.ptx >> %OUTFILE%
+bin2c.exe -st -n KERNELS_STRING_48 data\kernels48.ptx >> %OUTFILE%
+bin2c.exe -st -n KERNELS_STRING_LS_48 data\kernels48ls.ptx >> %OUTFILE%
+bin2c.exe -st -n KERNELS_STRING_64 data\kernels64.ptx >> %OUTFILE%
+bin2c.exe -st -n KERNELS_STRING_LS_64 data\kernels64ls.ptx >> %OUTFILE%
+
+echo #endif 	// __BeagleCUDA_kernels__ >> %OUTFILE%
+
