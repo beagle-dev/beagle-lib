677688c2cb143c381c263be7797ef972b77c8c55
aaron.darling <aaron.darling@6a382a6a-8faa-5fa0-d5e7-fc71f7735a90>
2009-10-22 00:31:04 +0000

    templating MEMCNV to eliminate some of the if(DOUBLE_PRECISION) statements

--- trunk/libhmsbeagle/CPU/Precision.h
+++ trunk/libhmsbeagle/CPU/Precision.h
@@ -10,11 +10,25 @@
 
 #define DOUBLE_PRECISION (sizeof(REALTYPE) == 8)
 
-#define MEMCNV(to, from, length, toType)    { \
+template<typename T, typename F> 
+inline void MEMCNV( T* to, F* from, unsigned int length )
+{
+	for(unsigned int m=0; m<length; m++)
+		to[m]=(T)from[m];
+};
+
+template<typename F> 
+inline void MEMCNV( F* to, const F* from, unsigned int length )
+{
+	memcpy( to, from, length*sizeof(F) );
+}
+
+/*#define MEMCNV(to, from, length, toType)    { \
                                                 int m; \
                                                 for(m = 0; m < length; m++) { \
                                                     to[m] = (toType) from[m]; \
                                                 } \
                                             }
+*/
 
 #endif /* PRECISION_H_ */
