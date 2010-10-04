a97996282c1ebcbceeeafae3de35ab773f8c48c9
Aaron Darling <koadman@users.noreply.github.com>
2010-10-04 10:47:44 +0000

    Adding an initial draft of a standalone example, including the GNU build framework

---
+++ examples/standalone/hellobeagle/src/hello.cpp
@@ -0,0 +1,16 @@
+/**
+ * An example hello world program for libhmsbeagle
+ * @author Aaron Darling
+ */
+
+#include <config.h>
+#include <libhmsbeagle/beagle.h>
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]){
+	// do the beagle, yeah!
+	cout << "Woof!\n";
+	return 0;
+}
