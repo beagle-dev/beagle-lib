5c4c2298aac09a78dc6a5a7245c9ce5747f560aa
fbielejec <fbielejec@6a382a6a-8faa-5fa0-d5e7-fc71f7735a90>
2012-01-26 10:35:33 +0000

    GPU convolution impl

--- trunk/libhmsbeagle/GPU/BeagleGPUImpl.h
+++ trunk/libhmsbeagle/GPU/BeagleGPUImpl.h
@@ -204,6 +204,15 @@ public:
     int getTransitionMatrix(int matrixIndex,
                             double* outMatrix);
 
+    ///////////////////////////
+    //---TODO: Epoch model---//
+    ///////////////////////////
+
+	int convolveTransitionMatrices(const int* firstIndices,
+                                   const int* secondIndices,
+                                   const int* resultIndices,
+                                   int matrixCount);
+
     int updateTransitionMatrices(int eigenIndex,
                                  const int* probabilityIndices,
                                  const int* firstDerivativeIndices,
