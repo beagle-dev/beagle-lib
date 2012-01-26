605cd853c030bd76df51d2d69889cfd7ad145c6f
Aaron Darling <koadman@users.noreply.github.com>
2012-01-26 01:34:26 +0000

    check for SSE at runtime, just in case somebody loaded our bitchin' software on their bitchin' 486.

--- libhmsbeagle/CPU/BeagleCPUSSEPlugin.cpp
+++ libhmsbeagle/CPU/BeagleCPUSSEPlugin.cpp
@@ -10,8 +10,13 @@
 #include "libhmsbeagle/CPU/BeagleCPUSSEImpl.h"
 #include <iostream>
 
+#ifdef __GNUC__
+#include "cpuid.h"
+#endif
+
 namespace beagle {
 namespace cpu {
+
 
 BeagleCPUSSEPlugin::BeagleCPUSSEPlugin() :
 Plugin("CPU-SSE", "CPU-SSE")
@@ -35,7 +40,6 @@ Plugin("CPU-SSE", "CPU-SSE")
 	// Optional for plugins: check if the hardware is compatible and only populate
 	// list with compatible factories and resources
 
-	// FIXME: the SSE plugin currently assumes all hardware is compatible
 	beagleFactories.push_back(new beagle::cpu::BeagleCPU4StateSSEImplFactory<double>());
 //	beagleFactories.push_back(new beagle::cpu::BeagleCPU4StateSSEImplFactory<float>()); // TODO Not yet written
 	beagleFactories.push_back(new beagle::cpu::BeagleCPUSSEImplFactory<double>()); // TODO In process of writing (disabled until it works for all input)
@@ -47,7 +51,66 @@ Plugin("CPU-SSE", "CPU-SSE")
 
 
 extern "C" {
+
+#ifdef _WIN32
+bool check_sse2(){
+    unsigned int features;
+
+    __asm
+    {
+        // Save registers
+        push    eax
+        push    ebx
+        push    ecx
+        push    edx
+
+        // Get the feature flags (eax=1) from edx
+        mov     eax, 1
+        cpuid
+        mov     features, edx
+
+        // Restore registers
+        pop     edx
+        pop     ecx
+        pop     ebx
+        pop     eax
+    }
+
+// Bit 26 for SSE2 support
+    return features & 0x04000000;
+}
+
+#endif
+
+#ifdef __GNUC__
+bool check_sse2()
+{
+  unsigned int eax, ebx, ecx, edx;
+  unsigned int ext, sig;
+
+  ext = 0;
+  __get_cpuid_max( ext, &sig );
+  printf( "ext=0x%x sig=0x%x\n", ext, sig );
+
+  if (!__get_cpuid (1, &eax, &ebx, &ecx, &edx)) {
+    printf( "__get_cpuid returned 0\n" );
+    return 0;
+  }
+
+  /* Run SSE2 test only if host has SSE2 support.  */
+  if (edx & bit_SSE2)
+	return true;
+
+	return false;
+}
+
+#endif
+
+
 void* plugin_init(void){
+	if(!check_sse2()){
+		return NULL;	// no SSE no plugin?! 
+	}
 	return new beagle::cpu::BeagleCPUSSEPlugin();
 }
 }
