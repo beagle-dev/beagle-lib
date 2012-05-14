1b5cb38519ca21cd97efde227f852975d4ba50fa
Filip Bielejec <filip.bielejec@rega.kuleuven.be>
2012-05-14 23:32:54 +0000

    epoch model standalone example

---
+++ examples/standalone/epochtest/config.h
@@ -0,0 +1,20 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "bug-report@hello.beagle-gpu.com"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "EpochTest C++"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "EpochTest C++ 0.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "hellobeagle"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://hello.beagle-gpu.com/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "0.1"
