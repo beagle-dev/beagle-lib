6e50ea7e5b6e4c77a64ec5b498e1aff7c565cf08
Daniel Ayres <ayresdl@users.noreply.github.com>
2013-08-06 14:55:48 +0000

    disabled out-of-order execution for OpenCL kernels (it failed some tests, need to investigate and potentially add further sync commands)

--- libhmsbeagle/GPU/GPUInterfaceOpenCL.cpp
+++ libhmsbeagle/GPU/GPUInterfaceOpenCL.cpp
@@ -220,7 +220,7 @@ void GPUInterface::SetDevice(int deviceNumber,
     openClContext = clCreateContext(NULL, 1, &openClDeviceId, NULL, NULL, &err);
     SAFE_CL(err);
     
-    cl_command_queue_properties queueProperties = CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE;
+    cl_command_queue_properties queueProperties = 0; //CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE;
     openClCommandQueue = clCreateCommandQueue(openClContext, openClDeviceId,
                                               queueProperties, &err);
     SAFE_CL(err);
