bebc8ea2af61fcdffffb21454f9f89ceec4b49a7
rambaut <rambaut@users.noreply.github.com>
2013-09-29 14:16:46 +0000

    Added getVersionInformation method to Java side. In API 2.0 this can call a library function.

--- java/beagle/BeagleJNIWrapper.java
+++ java/beagle/BeagleJNIWrapper.java
@@ -24,6 +24,16 @@ public class BeagleJNIWrapper {
     private BeagleJNIWrapper() {
     }
 
+    /**
+     * Currently not a native call but will be in BEAGLE API 2.0
+     * @return a string containing BEAGLE version info and citation
+     */
+    public String getVersionInformation() {
+        return "Using BEAGLE library v1.x for accelerated, parallel likelihood evaluation\n" +
+                "2009-2013, BEAGLE Working Group - http://beagle-lib.googlecode.com/\n" +
+                "Citation: Ayres et al (2012) Systematic Biology 61: 170-173 | doi:10.1093/sysbio/syr100\n";
+    }
+
     public native ResourceDetails[] getResourceList();
 
     public native int createInstance(
