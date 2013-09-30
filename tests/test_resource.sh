2378797f64bdf2440b0434a9a58ed75b5dd52092
Daniel Ayres <ayresdl@users.noreply.github.com>
2013-09-30 20:53:44 +0000

    fixed issue with OpenCL-CPU transition matrix derivative

--- tests/test_resource.sh
+++ tests/test_resource.sh
@@ -7,6 +7,8 @@ then
     ../examples/genomictest/genomictest --resourcelist
 else
     TEST_RESOURCE=${1}
+    echo "Testing resource..."
+    ../examples/genomictest/genomictest --resourcelist | grep "Resource $TEST_RESOURCE" -A 1 | grep -o --color=never ': .*'
     ./run_tests.sh $TEST_RESOURCE 2>&1 | grep "\*\*\*[^:]*:" -B 2
     echo "All tests completed."
 fi
