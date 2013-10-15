95cc4845520b948aef9ee626cf9de11d4cba8e40
daniel.ayres <daniel.ayres@6a382a6a-8faa-5fa0-d5e7-fc71f7735a90>
2013-10-15 12:03:24 +0000

    removing old intel-opencl-test branch in order to replace with up to date version from trunk

--- branches/intel-opencl-test/project/beagle-vs-2012/findJava.bat
+++
@@ -1,9 +0,0 @@
-IF DEFINED JDK_HOME (GOTO END) ELSE (GOTO FIND_JAVA_HOME)
-:FIND_JAVA_HOME
-echo Hunting for Java
-FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\SOFTWARE\JavaSoft\Java Development Kit" /v CurrentVersion') DO set CurVer=%%B
-FOR /F "skip=2 tokens=2*" %%A IN ('REG QUERY "HKLM\SOFTWARE\JavaSoft\Java Development Kit\%CurVer%"  /v JavaHome') DO set JDK_HOME=%%B
-:END
-
-echo #pragma include_alias(^<jni.h^>, ^<%JDK_HOME%\include\jni.h^>) > ..\..\libhmsbeagle\JNI\winjni.h
-echo #pragma include_alias("jni_md.h", "%JDK_HOME%\include\win32\jni_md.h") >> ..\..\libhmsbeagle\JNI\winjni.h
