fc0adbfc652e3207fbcb6f05cd0ccd537f148e14
Daniel L Ayres <ayres@umd.edu>
2017-03-15 13:40:01 +0000

    adjusted reported number of CUDA cores for latest Pascal microarchitecture GPUs

--- libhmsbeagle/GPU/GPUInterfaceCUDA.cpp
+++ libhmsbeagle/GPU/GPUInterfaceCUDA.cpp
@@ -71,14 +71,18 @@ inline int ConvertSMVer2CoresDRV(int major, int minor)
 
     sSMtoCores nGpuArchCoresPerSM[] =
     {
-        { 0x10,  8 }, // Tesla Generation (SM 1.0) G80 class
-        { 0x11,  8 }, // Tesla Generation (SM 1.1) G8x class
-        { 0x12,  8 }, // Tesla Generation (SM 1.2) G9x class
-        { 0x13,  8 }, // Tesla Generation (SM 1.3) GT200 class
         { 0x20, 32 }, // Fermi Generation (SM 2.0) GF100 class
         { 0x21, 48 }, // Fermi Generation (SM 2.1) GF10x class
         { 0x30, 192}, // Kepler Generation (SM 3.0) GK10x class
+        { 0x32, 192}, // Kepler Generation (SM 3.2) GK10x class
         { 0x35, 192}, // Kepler Generation (SM 3.5) GK11x class
+        { 0x37, 192}, // Kepler Generation (SM 3.7) GK21x class
+        { 0x50, 128}, // Maxwell Generation (SM 5.0) GM10x class
+        { 0x52, 128}, // Maxwell Generation (SM 5.2) GM20x class
+        { 0x53, 128}, // Maxwell Generation (SM 5.3) GM20x class
+        { 0x60, 64 }, // Pascal Generation (SM 6.0) GP100 class
+        { 0x61, 128}, // Pascal Generation (SM 6.1) GP10x class
+        { 0x62, 128}, // Pascal Generation (SM 6.2) GP10x class
         {   -1, -1 }
     };
 
@@ -93,7 +97,9 @@ inline int ConvertSMVer2CoresDRV(int major, int minor)
 
         index++;
     }
-    return nGpuArchCoresPerSM[7].Cores;
+
+    // If we don't find the values, we default use the previous one to run properly
+    return nGpuArchCoresPerSM[index-1].Cores;
 }
 
 }
