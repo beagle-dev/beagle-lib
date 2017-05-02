15d9165e81abe2d8ee5e7f7c84ed1ca5f3117210
Andrew Rambaut <rambaut@gmail.com>
2017-05-02 09:09:16 +0100

    Fixing flag declarations in enum.

--- java/beagle/BeagleFlag.java
+++ java/beagle/BeagleFlag.java
@@ -37,9 +37,9 @@ public enum BeagleFlag {
 
     FRAMEWORK_CUDA(1 << 22, "use CUDA implementation with GPU resources"),
     FRAMEWORK_OPENCL(1 << 23, "use OpenCL implementation with CPU or GPU resources"),
-    FRAMEWORK_CPU(1 << 27, "use CPU implementation");
+    FRAMEWORK_CPU(1 << 27, "use CPU implementation"),
 
-    PARALLELOPS_STREAMS(1 << 28, "Operations in updatePartials may be assigned to separate device streams");
+    PARALLELOPS_STREAMS(1 << 28, "Operations in updatePartials may be assigned to separate device streams"),
     PARALLELOPS_GRID(1 << 29, "Operations in updatePartials may be folded into single kernel launch (necessary for partitions; typically performs better for problems with fewer pattern sites)");
 
     BeagleFlag(long mask, String meaning) {
