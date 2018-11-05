db58564f6bc5cc945167add1b4f92617c8a13b31
Daniel L Ayres <ayres@umd.edu>
2018-11-05 11:48:21 +0000

    bug fix for  race condition with scale factor accumulation when using cuda streams, now using atomic functions

--- libhmsbeagle/GPU/kernels/kernelsAll.cu
+++ libhmsbeagle/GPU/kernels/kernelsAll.cu
@@ -41,6 +41,26 @@
     #define FMA(x, y, z) (z += x * y)
 #endif //FP_FAST_FMA
 
+#if (defined CUDA) && (defined DOUBLE_PRECISION) &&  (__CUDA_ARCH__ < 600)
+    __device__ double atomicAdd(double* address, double val)
+    {
+        unsigned long long int* address_as_ull =
+                                  (unsigned long long int*)address;
+        unsigned long long int old = *address_as_ull, assumed;
+
+        do {
+            assumed = old;
+            old = atomicCAS(address_as_ull, assumed,
+                            __double_as_longlong(val +
+                                   __longlong_as_double(assumed)));
+
+        // Note: uses integer comparison to avoid hang in case of NaN (since NaN != NaN)
+        } while (assumed != old);
+
+        return __longlong_as_double(old);
+    }
+#endif
+
 ///////////////////////////////////////////////////////////////////////////////
 
 KW_GLOBAL_KERNEL void kernelReorderPatterns(      KW_GLOBAL_VAR REAL*             dPartials,
