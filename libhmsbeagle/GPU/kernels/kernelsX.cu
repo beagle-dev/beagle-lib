d90e74ad260ded51128833d64822a1785847e8e1
Marc A. Suchard <msuchard@ucla.edu>
2019-04-25 19:41:15 +0200

    new kernel (although wrong) compiles; must code in both kernels4 and kernelsX

--- libhmsbeagle/GPU/kernels/kernelsX.cu
+++ libhmsbeagle/GPU/kernels/kernelsX.cu
@@ -374,12 +374,16 @@ KW_GLOBAL_KERNEL void kernelPartialsPartialsGrowing(KW_GLOBAL_VAR REAL* KW_RESTR
                                                     KW_GLOBAL_VAR REAL* KW_RESTRICT matrices1,
                                                     KW_GLOBAL_VAR REAL* KW_RESTRICT matrices2,
                                                     int totalPatterns) {
-// TODO This needs to rewritten
+#ifdef FW_OPENCL_CPU // CPU/MIC implementation
+    DETERMINE_INDICES_X_CPU();
+    SUM_PARTIALS_PARTIALS_X_CPU();
+    partials3[u] = sum1 * sum2;
+#else // GPU implementation
     DETERMINE_INDICES_X_GPU();
     SUM_PARTIALS_PARTIALS_X_GPU();
-    if (pattern < totalPatterns) {
+    if (pattern < totalPatterns)
         partials3[u] = sum1 *sum2;
-    }
+#endif
 }
 
 KW_GLOBAL_KERNEL void kernelPartialsPartialsNoScale(KW_GLOBAL_VAR REAL* KW_RESTRICT partials1,
