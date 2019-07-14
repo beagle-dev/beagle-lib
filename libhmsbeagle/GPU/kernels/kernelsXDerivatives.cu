fef8a701dc934076a3a26b7020eedfff0cbf931d
Marc A. Suchard <msuchard@ucla.edu>
2019-07-13 20:51:46 -0700

    clean code

--- libhmsbeagle/GPU/kernels/kernelsXDerivatives.cu
+++ libhmsbeagle/GPU/kernels/kernelsXDerivatives.cu
@@ -91,14 +91,11 @@ KW_GLOBAL_KERNEL void kernelPartialsStatesGrowing(KW_GLOBAL_VAR REAL* KW_RESTRIC
     DETERMINE_INDICES_X_GPU();
 
     KW_GLOBAL_VAR REAL* KW_RESTRICT matrix1 = matrices1 + deltaMatrix; /* Points to *this* matrix */
-//    KW_GLOBAL_VAR REAL* KW_RESTRICT matrix2 = matrices2 + deltaMatrix;
 
     /* Load values into shared memory */
     KW_LOCAL_MEM REAL sMatrix1[BLOCK_PEELING_SIZE][PADDED_STATE_COUNT];
-//    KW_LOCAL_MEM REAL sMatrix2[BLOCK_PEELING_SIZE][PADDED_STATE_COUNT];
 
     KW_LOCAL_MEM REAL sPartials1[PATTERN_BLOCK_SIZE][PADDED_STATE_COUNT];
-//    KW_LOCAL_MEM REAL sPartials2[PATTERN_BLOCK_SIZE][PADDED_STATE_COUNT];
 
     int y = deltaPartialsByState + deltaPartialsByMatrix;
 
@@ -106,31 +103,10 @@ KW_GLOBAL_KERNEL void kernelPartialsStatesGrowing(KW_GLOBAL_VAR REAL* KW_RESTRIC
     /* These are all coherent global memory reads; checked in Profiler */
     if (pattern < totalPatterns) {
         sPartials1[patIdx][state] = partials1[y + state];
-//        sPartials2[patIdx][state] = partials2[y + state];
     } else {
         sPartials1[patIdx][state] = 0;
-//        sPartials2[patIdx][state] = 0;
     }
-
-//    REAL sum2 = 0;
-//    for (int i = 0; i < PADDED_STATE_COUNT; i += BLOCK_PEELING_SIZE) {
-//        /* load one row of matrices */
-//        if (patIdx < BLOCK_PEELING_SIZE) {
-//            /* These are all coherent global memory reads. */
-//            sMatrix2[patIdx][state] = matrix2[patIdx * PADDED_STATE_COUNT + state];
-//            /* sMatrix now filled with starting in state and ending in i */
-//            matrix2 += BLOCK_PEELING_SIZE * PADDED_STATE_COUNT;
-//        }
-//
-//        KW_LOCAL_FENCE;
-//
-//        for(int j = 0; j < BLOCK_PEELING_SIZE; j++) {
-//            FMA(sMatrix2[j][state],  sPartials2[patIdx][i + j], sum2);
-//        }
-//
-//        KW_LOCAL_FENCE;
-//    }
-
+    
     REAL sum2 = 1;
     if (pattern < totalPatterns) { // Remove padded threads!
         int state2 = states2[pattern];
