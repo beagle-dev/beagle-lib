22502f3ff38d3ef81194d40cfe3807a448a159ca
gkarthik <xraydiffraction@gmail.com>
2024-02-29 03:10:05 -0600

    Permuted partials to remove memory bank conflicts for 4 state preorder.

--- libhmsbeagle/GPU/kernels/kernels4DerivativesTensorCore.cu
+++ libhmsbeagle/GPU/kernels/kernels4DerivativesTensorCore.cu
@@ -31,24 +31,26 @@ KW_GLOBAL_KERNEL void kernelPartialsPartialsGrowing(KW_GLOBAL_VAR REAL* KW_RESTR
     int warpIdx = warpState + warpPattern * 0.5; // blockDim.x is half a warp
     int laneId = tx + (patIdx % 2) * 16;
 
+    int ty = KW_LOCAL_ID_1;
+
 // Indices to permute ShM for partials
 // X -> threadIdx.x or state and Y -> threadIdx.y or patIdx
 // (int(X/8): Splits 32 values into groups of 4.
 // ((Y & 1) * -2 + 1)): For strip-mined layout: If patIdx is even increment by 1 else by -1
 // & 0x07 To cycle within the limits [0,1,2,3,4,5,6,7] i.e., [0, ... , PADDED_STATE_COUNT/WMMA_K]
-#define GET_SMEM_ROW_PARTIALS(X, Y) ( (X / contiguousX) & (totalWarps - 1))
-#define GET_BANK_GROUP_PARTIALS(X,Y) ((Y + (X/contiguousX) * ((Y & 1) * -2 + 1)) & (totalBankGroups - 1) )
+#define GET_SMEM_ROW_PARTIALS(X, Y) ( (X / contiguousX) + ((Y/4) * 4))
+#define GET_BANK_GROUP_PARTIALS(X,Y) ((Y + (X/contiguousX) * ((Y & 1) * -2 + 1)) & (4 - 1) )
 #define GET_SMEM_COL_PARTIALS(X,Y) (GET_BANK_GROUP_PARTIALS(X,Y) * contiguousX + (X % contiguousX))
-#define GET_SMEM_OFFSET_PARTIALS(X,Y) (GET_SMEM_ROW_PARTIALS(X, Y) * warpSize + GET_SMEM_COL_PARTIALS(X, Y))
+#define GET_SMEM_OFFSET_PARTIALS(X,Y) (GET_SMEM_ROW_PARTIALS(X, Y) * 16 + GET_SMEM_COL_PARTIALS(X, Y))
 
     /* copy PADDED_STATE_COUNT * PATTERN_BLOCK_SIZE lengthed partials*/
     if (pattern < endPattern) {
         // Read in permuted for partials1
-        sPartials1[ GET_SMEM_OFFSET_PARTIALS( laneId, warpIdx) ] = partials1[y | tx]; /*All coalesced memory*/
+        sPartials1[ GET_SMEM_OFFSET_PARTIALS( tx, ty) ] = partials1[y | tx]; /*All coalesced memory*/
 //      sPartials1Tmp[ multBy16(patIdx) | tx ] = partials1[y | tx];
         sPartials2[multBy16(patIdx) | tx] = partials2[y | tx];
     } else {
-        sPartials1[ GET_SMEM_OFFSET_PARTIALS( laneId, warpIdx) ] = 0;
+        sPartials1[ GET_SMEM_OFFSET_PARTIALS( tx, ty) ] = 0;
 //        sPartials1Tmp[multBy16(patIdx) | tx] = 0;
         sPartials2[multBy16(patIdx) | tx] = 0;
 //        tmpAcc[GET_SMEM_OFFSET_PARTIALS(tx % 4, patIdx * 4 + (tx / 4))] = 0;
@@ -87,6 +89,10 @@ KW_GLOBAL_KERNEL void kernelPartialsPartialsGrowing(KW_GLOBAL_VAR REAL* KW_RESTR
             : "d"(a2), "d"(b2), "d"(res21), "d"(res22));
 
     int partials1Index = (patIdx/2) * 32 + ((laneId * 2) % 8) * 4 + (laneId * 2) / WMMA_N;
+//    int partials1Tx1 = partials1Index % 16;
+//    int partials1Ty1 = partials1Index / 16;
+//    int partials1Tx2 = (partials1Index + 4) % 16;
+//    int partials1Ty2 = (partials1Index + 4) / 16;
     int partials1State1 = partials1Index % 16;
     int partials1PatIdx1 = partials1Index / 16;
     int partials1State2 = (partials1Index + 4) % 16;
@@ -96,10 +102,10 @@ KW_GLOBAL_KERNEL void kernelPartialsPartialsGrowing(KW_GLOBAL_VAR REAL* KW_RESTR
 //        int warpPattern = patIdx;
 //        int warpIdx = warpState + warpPattern * 0.5; // blockDim.x is half a warp
 //        int laneId = tx + (patIdx % 2) * 16;
-    int partials1WarpIdx1 = (partials1State1 / warpSize) + partials1PatIdx1 * 0.5;
-    int partials1LaneId1 = partials1State1 + (partials1PatIdx1 % 2) * 16;
-    int partials1WarpIdx2 = (partials1State2 / warpSize) + partials1PatIdx2 * 0.5;
-    int partials1LaneId2 = partials1State2 + (partials1PatIdx2 % 2) * 16;
+//    int partials1WarpIdx1 = (partials1State1 / warpSize) + partials1PatIdx1 * 0.5;
+//    int partials1LaneId1 = partials1State1 + (partials1PatIdx1 % 2) * 16;
+//    int partials1WarpIdx2 = (partials1State2 / warpSize) + partials1PatIdx2 * 0.5;
+//    int partials1LaneId2 = partials1State2 + (partials1PatIdx2 % 2) * 16;
 
 
 
@@ -110,8 +116,8 @@ KW_GLOBAL_KERNEL void kernelPartialsPartialsGrowing(KW_GLOBAL_VAR REAL* KW_RESTR
 //        sPartials1Tmp[partials1Index] = sPartials1Tmp[partials1Index] * res21;
 //        sPartials1Tmp[partials1Index + 4] = sPartials1Tmp[partials1Index + 4] * res22;
 
-        sPartials1[ GET_SMEM_OFFSET_PARTIALS( partials1LaneId1, partials1WarpIdx1) ] = sPartials1[ GET_SMEM_OFFSET_PARTIALS( partials1LaneId1, partials1WarpIdx1) ] * res21;
-        sPartials1[ GET_SMEM_OFFSET_PARTIALS( partials1LaneId2, partials1WarpIdx2) ] = sPartials1[ GET_SMEM_OFFSET_PARTIALS( partials1LaneId2, partials1WarpIdx2) ] * res22;
+        sPartials1[ GET_SMEM_OFFSET_PARTIALS( partials1State1, partials1PatIdx1 ) ] = sPartials1[ GET_SMEM_OFFSET_PARTIALS( partials1State1, partials1PatIdx1) ] * res21;
+        sPartials1[ GET_SMEM_OFFSET_PARTIALS( partials1State2, partials1PatIdx2) ] = sPartials1[ GET_SMEM_OFFSET_PARTIALS( partials1State2, partials1PatIdx2) ] * res22;
 
     }
 
@@ -128,10 +134,10 @@ KW_GLOBAL_KERNEL void kernelPartialsPartialsGrowing(KW_GLOBAL_VAR REAL* KW_RESTR
     partials1Index = warpIdx * 32 + reg_col_partials * 16 + reg_row_partials;
     int partials1State = partials1Index % 16;
     int partials1PatIdx = partials1Index / 16;
-    int partials1WarpIdx = ((partials1State / warpSize) + partials1PatIdx * 0.5);
-    int partials1LaneId = (partials1State + (partials1PatIdx % 2) * 16);
+//    int partials1WarpIdx = ((partials1State / warpSize) + partials1PatIdx * 0.5);
+//    int partials1LaneId = (partials1State + (partials1PatIdx % 2) * 16);
 
-    b1 = sPartials1[GET_SMEM_OFFSET_PARTIALS(partials1LaneId, partials1WarpIdx)];
+    b1 = sPartials1[GET_SMEM_OFFSET_PARTIALS(partials1State, partials1PatIdx)];
 
     asm("mma.sync.aligned.m8n8k4.row.col.f64.f64.f64.f64 {%0,%1}, {%2}, {%3}, {%4,%5};\n"
             : "=d"(res11), "=d"(res12)
