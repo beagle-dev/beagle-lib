543ae9a5877cad3eebede49a9d14fda1dbe7db74
Benjamin Redelings <benjamin.redelings@gmail.com>
2024-03-19 13:25:22 -0400

    Also clear powerMatrices

--- libhmsbeagle/CPU/BeagleCPUActionImpl.hpp
+++ libhmsbeagle/CPU/BeagleCPUActionImpl.hpp
@@ -370,6 +370,7 @@ namespace beagle {
             gInstantaneousMatrices[eigenIndex].setFromTriplets(tripletList.begin(), tripletList.end());
             gHighestPowers[eigenIndex] = 0;
             ds[eigenIndex].clear();
+            powerMatrices[eigenIndex].clear();
 
             double mu_B = 0.0;
             for (int i = 0; i < kStateCount; i++) {
